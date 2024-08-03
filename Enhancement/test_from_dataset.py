47468918d6f6a0c6fd24287641cea004b30f87db
Tai Pham <39149250+taipt03@users.noreply.github.com>
2024-08-03 14:59:28 +0700

    Update test_from_dataset.py

--- Enhancement/test_from_dataset.py
+++ Enhancement/test_from_dataset.py
@@ -1253,6 +1253,9 @@ else:
     
     classification_model = EfficientNet.from_name('efficientnet-b0')
     classification_model.load_state_dict(torch.load("/kaggle/input/efficientnet-chen/pytorch/default/5/best_model.pth"))
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.to(device)
+
     classification_model.eval()
 
     # Load and process the image
