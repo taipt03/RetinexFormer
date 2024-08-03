e1487fdf76978ac8fea112064326a50e8234932f
Tai Pham <39149250+taipt03@users.noreply.github.com>
2024-08-03 12:41:20 +0700

    Update test_from_dataset.py

--- Enhancement/test_from_dataset.py
+++ Enhancement/test_from_dataset.py
@@ -1251,9 +1251,8 @@ else:
     single_image_path = '/kaggle/input/chen-vcor/data/data/black/007ab0bb16.jpg'  
 
     
-    classification_model_path = '/kaggle/input/efficientnet-chen/pytorch/default/4/best_model-94.pth' 
     classification_model = EfficientNet.from_name('efficientnet-b0')
-    classification_model.load_state_dict(torch.load("/kaggle/input/efficientnet-chen/pytorch/default/3/best.pth"))
+    classification_model.load_state_dict(torch.load("/kaggle/input/efficientnet-chen/pytorch/default/4/best_model-94.pth"))
     classification_model.eval()
 
     # Load and process the image
